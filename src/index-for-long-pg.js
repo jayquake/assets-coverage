325c904b1fdeed9ec182e433a06e5fec79919e92
Jason Quaicoo <jayquake@gmail.com>
2025-03-25 14:05:41 +0000

    New updates for Re Reouting

--- src/index-for-long-pg.js
+++ src/index-for-long-pg.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "../public/styles.css";
 //import "./assets/files/secondfile-name-file-pdf-file-sample_PDFexample-of-a-really-long-file-name-file-pdf-filesample_PDFexample237891a.pdf";
 document.getElementById("app").innerHTML = `
 
